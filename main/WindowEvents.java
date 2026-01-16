b792da9074aa0168578a9b5309b51959242380af
Kenneth <barotillakenneth@gmail.com>
2026-01-16 16:45:24 +0800

    Added GUI classes. WindowEvents need to be updated.

---
+++ main/WindowEvents.java
@@ -0,0 +1,11 @@
+package main;
+
+public class WindowEvents {
+    // Method for matrix size input >= 2 && <= 8 ------ -> matrixSize will nested for loop textfield until n*n. each box is saved to matrixBoxValue[i][j] 
+
+    // Method for addlistener for inversion method buttons. 
+
+    // Method for addllistener for calculate. Call gaussian elimination if RREF, not sure about adjoint.
+    
+    // Method for addlistener for clear, clearing all inputs to default
+}
